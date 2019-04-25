ac370666a90adc5792f2df3246eada98f3a41e5b
po@becode <dezutterpablo@gmail.com>
2019-04-25 15:27:41 +0200

    asv-confirm

---
+++ 01-base/04-asv-confirm/script.js
@@ -0,0 +1,38 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+var age= prompt("What is your age?");
+var gender= prompt("What is your gender?");
+var location= prompt("Where do you live?");
+
+if(age !== null){
+    var gender;
+
+}else{
+    var age;
+
+}
+if(gender!== null){
+    var location;
+}else{
+    var age;
+}
+if(location!== null){
+ alert('thank you')
+}else{
+    var age;
+}
+
+
+})();
