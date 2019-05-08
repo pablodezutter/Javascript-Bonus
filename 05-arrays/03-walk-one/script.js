9143db8ac7db097ed74bfe386961dbeeac52bd9f
po@becode <dezutterpablo@gmail.com>
2019-05-08 10:35:13 +0200

    walk one

---
+++ 05-arrays/03-walk-one/script.js
@@ -0,0 +1,44 @@
+/* becode/javascript
+ *
+ * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var fruits = [
+        "apple",
+        "pear",
+        "raspberry",
+        "tomatos",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peache",
+        "grap",
+        "cherry",
+    ];
+   
+
+    // your code here
+var Btn= document.getElementById("run");
+Btn.addEventListener("click",function(){
+
+for(i=0; i < fruits.length; i++){
+
+console.log(fruits[i]);
+
+ 
+    }
+});
+
+
+
+})();
