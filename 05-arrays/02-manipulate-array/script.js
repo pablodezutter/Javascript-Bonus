a77aea096b647899c47a5afeed90210ec58174b7
po@becode <dezutterpablo@gmail.com>
2019-05-08 09:44:58 +0200

    manipulate array

---
+++ 05-arrays/02-manipulate-array/script.js
@@ -0,0 +1,43 @@
+/* becode/javascript
+ *
+ * /05-arrays/02-manipulate-array/script.js - 5.2: manipulation de tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var fruits = [
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "durian",
+        "pêche",
+        "raisin",
+        "cerise",
+    ];
+
+    // your code here
+var Btn = document.getElementById("run");
+
+Btn.addEventListener("click",function(){
+console.log('pomme');
+fruits.splice(0,1,'banana');
+fruits.splice(9,1,'kiwi');
+console.log(fruits);
+
+
+
+
+});
+
+
+
+})();
