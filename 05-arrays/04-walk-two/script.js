5072f639ec92e9cf869851b93ce5998199c7b79e
po@becode <dezutterpablo@gmail.com>
2019-05-08 10:43:38 +0200

    walk two

---
+++ 05-arrays/04-walk-two/script.js
@@ -0,0 +1,43 @@
+/* becode/javascript
+ *
+ * /05-arrays/04-walk-two/script.js - 5.4: parcours de tableau (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var fruits = [
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "kiwi",
+        "banane",
+        "orange",
+        "mandarine",
+        "durian",
+        "pêche",
+        "raisin",
+        "cerise",
+    ];
+
+    // your code here
+    var Btn= document.getElementById("run");
+
+    Btn.addEventListener("click", function(){
+        fruits.forEach(function(elements){
+            console.log(elements);
+
+
+        });
+
+
+
+    });
+
+})();
